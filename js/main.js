13c70c6e5eadde0779f390c65f69ab1ad05489be
Maria <maria_suxareva@mail.ru>
2019-06-21 12:31:34 +0300

    Dev version

--- js/main.js
+++ js/main.js
@@ -5,40 +5,19 @@ var parallaxInstance = new Parallax(scene, {
 });
 
 $(function () {
-    $(document).on("click", ".logo", function () {
-        if ($(".main").hasClass("rus")) {
-            $(".logo_eng").removeClass("hide");
-            $(".main")
-                .toggleClass("rus")
-                .toggleClass("eng");
-            $(".category__item.item1").html("IT");
-            $(".category__item.item2").html("Highload Dev");
-            $(".category__item.item3").html("Big Data Analytics");
-            $(".copyrite").html("© Uma.Tech LLC 2019");
-        } else {
-            $(".main")
-                .toggleClass("rus")
-                .toggleClass("eng");
-            $(".category__item.item1").html("ИТ");
-            $(".category__item.item2").html(
-                "Разработка высоконагруженных<br> сервисов"
-            );
-            $(".category__item.item3").html("Аналитика больших<br> данных");
-            $(".copyrite").html("© ООО “Ума.Тех”  2019");
-        }
-    });
     var catHeight = $(".category").height() + 10;
     var catWidth = $(".content_center__block").width();
-
-    $(".category").css("min-height", catHeight);
-    $(".content_center__block").css("min-width", catWidth);
-    $(".footer").css("min-width", catWidth);
-
-    $(window).resize(function () {
-        var catHeight = $(".category").height();
-        var catWidth = $(".content_center__block").width();
-        $(".category").css("min-height", catHeight);
-        $(".content_center__block").css("min-width", catWidth);
-        $(".footer").css("min-width", catWidth);
-    });
 });
+
+var formURL =
+  "https://docs.google.com/forms/d/15Ts8au3uporAAOmcPJAcHTpK91t2kIAjej0djfHfxE4/formResponse?";
+
+sendBtn.onclick = function() {
+  var data = {
+    "entry.779126364": document.getElementById("entry.779126364").value,
+    "entry.1039583708": document.getElementById("entry.1039583708").value
+  };
+  $("#form").hide();
+  postToGoogle(formURL, data);
+  // loadSheet.click();
+};
